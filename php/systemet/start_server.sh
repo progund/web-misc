4503d8f634c2b00a16f06638c49be6d4db15bd1b
Rikard Fröberg <rikard@morus.se>
2017-11-15 14:22:10 +0100

    A script for starting a PHP server in the terminal

---
+++ php/systemet/start_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting PHP server on http://localhost:8088"
+echo
+echo "Visit http://localhost:8088/search.html in order to try the search form."
+echo
+echo
+php -S localhost:8088
+echo "Server shut down"
