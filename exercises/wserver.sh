7a80996284806facc45e0c22040ed6894cbd3987
Henrik Sandklef <hesa@sandklef.com>
2017-11-18 21:22:47 +0100

    first rev

---
+++ exercises/wserver.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+#
+# Function to output header
+#
+header()
+{
+    echo "HTTP/1.0 200 OK"
+    echo "Connection: close"
+    echo "Date: $(date)"
+    echo "Server: netcat special deal"
+    echo "Content-Type: text/html; charset=utf-8"
+    echo "Cache-Control: max-age=60"
+    echo ""
+    echo ""
+}
+
+#
+# Function to outout content 
+#
+content()
+{
+    echo "<html>"
+    echo "<head><title>Some web page title</title></head>"
+    echo "<body>"
+    echo "<p>Hello HTTP</p>"
+    echo "</body>"
+    echo "</html>"
+}
+
+header
+content
+
+
