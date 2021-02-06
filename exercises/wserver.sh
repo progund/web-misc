2a17030993f1a879c46d6bd404213f55b9dda4ca
Rikard Fröberg <rikard@morus.se>
2021-02-06 12:28:31 +0100

    Sets date header correctly

--- exercises/wserver.sh
+++ exercises/wserver.sh
@@ -21,20 +21,19 @@ header()
 {
     echo "HTTP/1.1 200 OK"
     echo "Connection: close"
-    echo "Date: $(date)"
+    #  All HTTP date/time stamps MUST be represented
+    #+ in Greenwich Mean Time (GMT)
+    echo "Date: $(TZ=Etc/GMT LC_TIME=C date '+%a, %d %b %Y %T %Z')"
     echo "Server: netcat special deal"
     echo "Content-Length: $LENGTH"
     echo "Content-Type: text/html; charset=utf-8"
     echo "Cache-Control: max-age=60"
     echo ""
-    echo ""
-    echo ""
 }
 
 CONTENT=$(content)
-CLENGTH=$(echo $CONTENT | wc -c)
-LENGTH=$(( $CLENGTH + 1 ))
+CLENGTH=$(echo -n $CONTENT | wc -c)
+LENGTH=$(( $CLENGTH ))
 header
-echo ${CONTENT}
-exec 1>&-
-
+echo -n ${CONTENT}
+#exec 1>&-
